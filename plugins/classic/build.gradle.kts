97417e50d2898a2150586ffdc61ef2319abb27a3
James Grunewald <james.grunewald@jrfg.uk>
2024-09-02 02:41:04 +0100

    Removed ktx-inject dependency

--- plugins/classic/build.gradle.kts
+++ plugins/classic/build.gradle.kts
@@ -6,13 +6,10 @@ plugins {
 
 dependencies {
     val pf4jVersion: String by rootProject.extra
-    val ktxVersion: String by rootProject.extra
 
     compileOnly(project(":api"))
     compileOnly(kotlin("stdlib"))
 
     compileOnly("org.pf4j:pf4j:${pf4jVersion}")
     kapt("org.pf4j:pf4j:${pf4jVersion}")
-
-    api("io.github.libktx:ktx-inject:$ktxVersion")
 }
